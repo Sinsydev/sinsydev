1e14bb727ef4cd8a4c80ae1452cf5502906a048e
sinsydev <sinsyfrontenddeveloper@gmail.com>
2025-08-26 16:49:13 +0100

    fixed

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
 
 
 const path = './data.json';
 const date = moment().format();
 
 
+const makeCommits = (n) => {
+    const x = random.default.int(0,54);
+    const y = random.default.int(0,6);
+    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+}
+
+
 const data = {
     date: date,
 };
 
-jsonfile.writeFile(path, data);
+jsonfile.writeFile(path, data,()=> {
+
+    simpleGit().add([path]).commit(date,{'--date' :date}).push();
+});
+
