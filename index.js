0f5eb59deda6d948f8c84a14c1cf5adfd246bc40
sinsydev <sinsyfrontenddeveloper@gmail.com>
2025-08-26 16:56:06 +0100

    fixed

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ const date = moment().format();
 
 
 const makeCommits = (n) => {
+    if (n===0) return simpleGit.push();
     const x = random.default.int(0,54);
     const y = random.default.int(0,6);
     const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
-}
 
 
 const data = {
     date: date,
 };
-
+console.log(date);
 jsonfile.writeFile(path, data,()=> {
-
-    simpleGit().add([path]).commit(date,{'--date' :date}).push();
+    simpleGit().add([path]).commit(date,{'--date' :date},makeCommits.bind(this,--n));
 });
 
+};
+
+makeCommits(150);
